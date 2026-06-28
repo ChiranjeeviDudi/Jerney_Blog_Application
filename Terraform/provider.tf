27ee2b6ec4c5c28340f21102af498ec511f3d97d
Chiranjeevi Dudi <chiranjeevidudi3005@gmial.com>
2026-06-28 11:49:27 +0530

    updated tf files

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "jerney-terraform-state"
+    bucket         = "jerney-terraform-statefile"
     key            = "auto_eks/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "jerney-tf-lock"
