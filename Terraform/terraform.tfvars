9ba3450fd0d4c7be7632d83fc3b3cfa55910e8f7
Chiranjeevi Dudi <chiranjeevidudi3005@gmial.com>
2026-06-28 12:13:19 +0530

    updated tf files

---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region      = "ap-south-1"
+environment     = "dev"
+cluster_name    = "jerney-eks-cluster"
+cluster_version = "1.35"
+vpc_cidr        = "10.0.0.0/16"
---
+++ Terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  description = "AWS region where we are going to deploy EKS cluster"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "jerney-eks-cluster"
+}
+
+variable "cluster_version" {
+  description = "cluster _version"
+  type        = string
+  default     = "1.35"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
